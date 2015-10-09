6f1fcd82315eadaa10e784e8fa2284edaf008e46
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-09 12:00:24 +0100

    Game returns draw status

---
+++ src/main/java/ttt/Game.java
@@ -0,0 +1,10 @@
+package ttt;
+
+/**
+ * Created by Georgina on 09/10/15.
+ */
+public class Game {
+    public String play() {
+        return "Draw";
+    }
+}
--- src/test/java/ttt/TicTacToeTest.java
+++ src/test/java/ttt/TicTacToeTest.java
@@ -2,12 +2,20 @@ package ttt;
 
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
 /**
  * Created by Georgina on 09/10/15.
  */
 public class TicTacToeTest {
 
     @Test
-    public void theGameStartsWithAnEmptyGrid() {
+    public void theGameIsDrawnWhenThereIsNoWinner() {
+        Game game = new Game();
+
+        String status = game.play();
+
+        assertThat(status, is("Draw"));
     }
 }
