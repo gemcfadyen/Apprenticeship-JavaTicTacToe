f03f6cd10e0ade1c1a1d88bc81b7f463677618d9
gemcfadyen <gemcfadyen@hotmail.co.uk>
2015-10-09 17:18:55 +0100

    Refactors identification of row into a method

--- src/main/java/ttt/Board.java
+++ src/main/java/ttt/Board.java
@@ -17,21 +17,33 @@ public class Board {
     }
 
     public boolean containsWinningRow() {
-        if (symbols[0] == "X"
-                && symbols[1] == "X"
-                && symbols[2] == "X") {
+        if (topRowHasMatchingSymbols()) {
             return true;
-        } else if (symbols[6] == "X"
-                && symbols[7] == "X"
-                && symbols[8] == "X") {
+        } else if (middleRowHasMatchingSymbols()) {
             return true;
-        } else if (symbols[3] == "X"
-                && symbols[4] == "X"
-                && symbols[5] == "X") {
+        } else if (bottomRowHasMatchingSymbols()) {
             return true;
         }
 
         return false;
+    }
+
+    private boolean bottomRowHasMatchingSymbols() {
+        return symbols[6] == "X"
+                && symbols[7] == "X"
+                && symbols[8] == "X";
+    }
+
+    private boolean middleRowHasMatchingSymbols() {
+        return symbols[3] == "X"
+                && symbols[4] == "X"
+                && symbols[5] == "X";
+    }
+
+    private boolean topRowHasMatchingSymbols() {
+        return symbols[0] == "X"
+                && symbols[1] == "X"
+                && symbols[2] == "X";
     }
 
 }
